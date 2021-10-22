cac23b82f26cef382c267ddb8d2c2108e3bfc574
tziphoro <tziphoro@gmail.com>
2021-10-21 21:07:03 -0400

    trainer and database updated

--- bot_trainer.py
+++ bot_trainer.py
@@ -6,23 +6,19 @@ bot = ChatBot(
     "Chutter",
     logic_adapters=['chatterbot.logic.BestMatch'],
     preprocessors=['chatterbot.preprocessors.clean_whitespace'],
-    database_uri='sqlite:///./bot.db'
+    database_uri='sqlite:///./chutter.db'
 )
 
-trainer2 = ChatterBotCorpusTrainer(bot)
-trainer2.train(
-    "chatterbot.corpus.english",
-)
-
-trainer = ListTrainer(bot)
-chat1 = read_csv(r'C:\Users\rebec\WhatsappBot\Response_Bot\chats\chat1.txt', delimiter=',')['message'].astype(str).to_list()
-chat2 = read_csv(r'C:\Users\rebec\WhatsappBot\Response_Bot\chats\chat2.txt', delimiter=',')['message'].astype(str).to_list()
-chat3 = read_csv(r'C:\Users\rebec\WhatsappBot\Response_Bot\chats\chat3.txt', delimiter=',')['message'].astype(str).to_list()
-# chat4 = read_csv(r'C:\Users\rebec\WhatsappBot\Response_Bot\chats\chat4.txt', delimiter=',')['message'].astype(str).to_list()
-train_data = chat1 + chat2 + chat3
-trainer.train(train_data)
-
-bot.get_response('My name is Tziporah')
+# trainer = ChatterBotCorpusTrainer(bot)
+# trainer.train(
+#     "chatterbot.corpus.english",
+# )
+#
+# trainer2 = ListTrainer(bot)
+# chat1 = read_csv(r'path\to\chat.csv', delimiter=',')['message'].astype(str).to_list()
+# trainer2.train(chat1)
+#
+# bot.get_response('My name is Tziporah')
 
 while True:
     response = input('Tziporah: ')
